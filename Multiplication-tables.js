1af76b1310ea1529841290030fb31a5b6cf13607
SamLinoFinnegan <73761381+SamLinoFinnegan@users.noreply.github.com>
2020-11-28 17:58:18 +0000

    Add files via upload

---
+++ Multiplication-tables.js
@@ -0,0 +1,6 @@
+let numb = 0;
+while (numb <= 10) {
+    let res = numb * 9;
+    console.log(numb + " X 9 =" + res)
+    numb++
+}
---
+++ even-odd-reporter.js
@@ -0,0 +1,11 @@
+let a = 0;
+while (a < 21) {
+    if (a % 2 == 1) {
+        console.log(a + " is odd");
+
+    } else if (a % 2 == 0) {
+        console.log(a + " is even")
+    }
+    a++;
+}
+console.log("those were the numbers from 0 till 20")
